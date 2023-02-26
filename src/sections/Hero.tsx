37dfbaf6e41474b6e0916c67fce672436625db4d
Dzikri Syairozi <85748069+dzikrisyairozi@users.noreply.github.com>
2023-02-26 13:02:38 +0700

    Add files via upload

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,27 @@
+import ButtonLink from '@/components/ButtonLink'
+import Counter from '@/components/Counter'
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section id='banner' className="bg-[url('/bg.jpg')] bg-cover bg-no-repeat">
+    <div className='layout flex flex-col gap-y-3 justify-center min-h-screen text-center bg-[#00000075]'>
+      <h1 className='text-6xl font-bold text-white'>
+        Demo
+      </h1>
+      <p className='text-xl font-normal text-gray-200'>
+        React Hook Form
+      </p>
+      <div className='flex justify-center gap-3'>
+        <ButtonLink link='get-started' title='Get Started'/>
+        <ButtonLink link='register' title='Register'/>
+        <ButtonLink link='demo' title='Demo'/>
+        {/* <ButtonLink  title='Learn More'/> */}
+        {/* <Counter/> */}
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default Hero
